8f88e71d0cbd0903d78b7171c7b958bd05923607
DiegoCaroFrechilla <146827294+DiegoCaroFrechilla@users.noreply.github.com>
2024-10-28 15:45:23 +0100

    extra

--- main.ts
+++ main.ts
@@ -1,4 +1,14 @@
+let setRandom2 = 0
 let receivedString2 = 0
+let setRandom = 0
+radio.onReceivedNumber(function (receivedNumber) {
+    setRandom2 = randint(1, 6)
+    if (receivedNumber < setRandom2) {
+        basic.showIcon(IconNames.Happy)
+    } else {
+        basic.showIcon(IconNames.Sad)
+    }
+})
 input.onButtonPressed(Button.A, function () {
     radio.sendString("HELLO")
 })
@@ -7,4 +17,8 @@ radio.onReceivedString(function () {
 })
 radio.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
+})
+input.onButtonPressed(Button.B, function () {
+    setRandom = randint(1, 6)
+    radio.sendNumber(setRandom)
 })
