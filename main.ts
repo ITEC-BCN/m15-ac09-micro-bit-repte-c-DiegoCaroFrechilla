ac67ea0725906bdcc04babdc82219fe76a8d9700
DiegoCaroFrechilla <146827294+DiegoCaroFrechilla@users.noreply.github.com>
2024-10-28 15:38:09 +0100

    Hello

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
-basic.forever(function () {
-	
+let receivedString2 = 0
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("HELLO")
+})
+radio.onReceivedString(function () {
+    basic.showString("" + (receivedString2))
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
 })
